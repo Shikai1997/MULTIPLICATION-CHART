424e53d4fc4bb95f75b31bcada7ca0fb91b2aad6
Shikai1997 <56082441+Shikai1997@users.noreply.github.com>
2020-03-19 14:24:01 +0800

    Create all.js

---
+++ all.js
@@ -0,0 +1,19 @@
+let container = document.querySelector('.container');
+let firest = `<div class="first">
+<div class="txt">
+  <div class="title">九九乘法表</div>
+  <p>MULTIPLICATION CHART</p>
+</div>
+</div>`;
+
+let str = '';
+
+for(let i = 2; i<10 ; i++){
+    str += `<div class="card"><h3>${i}</h3>`;
+    for(let j = 1; j<10 ; j++){
+        str += `<li>${i} x ${j} = ${i*j}</li>`
+    }
+    str +=`</div>`
+}
+container.innerHTML = firest + str;
+Template literals
